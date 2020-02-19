ee2e746c8f78ae982ec4e67fee820b79ab75ef4e
Joshua Kennedy <josh@outthinkgroup.com>
2020-02-19 09:03:19 -0500

    trivia prototype

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,58 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export const API = "https://opentdb.com/api.php?amount=1"
+const IndexPage = () => {
+  const [category, setCategory] = useState(null)
+  const [questionData, setQuestionData] = useState(null)
+
+  const getQuestion = async e => {
+    e.preventDefault()
+    const res = await fetch(`${API}`)
+    const data = await res.json()
+    setQuestionData(data.results)
+    console.log(questionData)
+  }
+  function getAnswers(questionObj) {
+    const { incorrect_answers, correct_answer } = questionObj
+    if ((questionObj.type = "multiple")) {
+      const answers = [...incorrect_answers, correct_answer]
+      return answers.sort(() => Math.random() - 0.5)
+    } else {
+      const answers = [incorrect_answers, correct_answer]
+      return answers.sort(() => Math.random() - 0.5)
+    }
+  }
+
+  const possibleAnswers = questionData && getAnswers(questionData[0])
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      {questionData && (
+        <>
+          <h3 dangerouslySetInnerHTML={{ __html: questionData[0].question }} />
+          <ul>
+            {possibleAnswers.map((answer, key) => (
+              <li key={key}>{answer}</li>
+            ))}
+          </ul>
+          <details>
+            <summary>answer</summary>
+            {questionData[0].correct_answer}
+          </details>
+        </>
+      )}
+      <div>
+        <button onClick={getQuestion}>get a questino</button>
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
